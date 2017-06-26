d87a92698d9aedeb80d94f1d198b4fe05874967e
Oddvar Lia <olia@statoil.com>
2017-06-26 11:26:43 +0200

    Added missing files to build the libdraw2D.so which is necessary to make 2D simulation available as python function.

---
+++ src/lib/libgaussField/definitions.h
@@ -0,0 +1,11 @@
+// global definitions in barrier project
+
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+typedef double real;
+#define PI  3.14159265358979323846
+//#define IGNORE_FACIES_TRUNCATION 1
+#define ALMOST_ZERO 0.0005
+//#define GAUSS_VALUE
+#endif
