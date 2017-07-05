63f714a5bbecd730dbb96614ea64383904bc1c93
Sindre Nistad <snis@statoil.com>
2017-07-05 14:38:58 +0200

    feat: Installs Python bindings for QCustomPlot

--- libraries/PyQt.sh
+++ libraries/PyQt.sh
@@ -5,8 +5,12 @@ VENV="$(pwd)/../.venv"
 # Using the virtual environment
 source ./../.venv/bin/activate
 
-QT_PREFIX="/opt/Qt/5.9.1/gcc_64/bin"
-PATH=${QT_PREFIX}:${PATH}
+QT_PREFIX="/opt/Qt/5.9.1/gcc_64"
+QT_BIN_DIR="${QT_PREFIX}/bin"
+QT_INCLUDE_DIR="${QT_PREFIX}/include"
+QT_LIBRARY_PATH="${QT_PREFIX}/lib"
+QMAKE_PATH="${QT_BIN_DIR}/qmake"
+PATH=${QT_BIN_DIR}:${PATH}
 
 # Setting the working directory
 SOURCE=$(pwd)/source
@@ -31,7 +35,7 @@ PYQT_GPL_PREFIX="PyQt5_gpl-${PYQT_VERSION}"
 wget https://sourceforge.net/projects/pyqt/files/PyQt5/${PYQT_PREFIX}/${PYQT_GPL_PREFIX}.tar.gz
 tar -xvf ${PYQT_GPL_PREFIX}.tar.gz
 cd ${PYQT_GPL_PREFIX}
-python configure.py --sysroot "${VENV}" --qmake=${QT_PREFIX}/qmake  --confirm-license
+python configure.py --sysroot "${VENV}" --qmake=${QMAKE_PATH}  --confirm-license
 make
 make install
 
@@ -47,6 +51,12 @@ cd ${SOURCE}
 #
 #cd ${SOURCE}
 
+# Download and install QCustomPlot for python
+export LD_LIBRARY_PATH=${QT_LIBRARY_PATH}:${LD_LIBRARY_PATH}
+export LIBRARY_PATH=${QT_LIBRARY_PATH}:${LIBRARY_PATH}
+git clone https://github.com/dimv36/QCustomPlot-PyQt5.git
+cd QCustomPlot-PyQt5
+python setup.py build_ext --qmake "${QMAKE_PATH}" --qt-include-dir "${QT_INCLUDE_DIR}"
 
 # Clean up
 rm -f *.tar.*
