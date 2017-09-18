93b47a440f2cf7bb30dd351b68b32c803106acba
Sindre Nistad <snis@statoil.com>
2017-09-18 18:45:24 +0200

    chore: Cleaned up the main entry point of the GUI

--- src/gui/app.py
+++ src/gui/app.py
@@ -4,15 +4,12 @@ import sys
 
 from PyQt5.QtWidgets import QApplication
 
-from src.gui.wrappers.main_window import MainWindow
 from src.gui.wrappers.project import Project
 
 
 def run():
     app = QApplication(sys.argv)
-    # zone_parameter_name = QMainWindow()
     ui = Project()
-    # ui.setupUi(zone_parameter_name)
     ui.show()
     sys.exit(app.exec_())
 
