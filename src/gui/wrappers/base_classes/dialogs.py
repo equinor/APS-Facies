5dc16169cf4afd7333fc8ce23e6ffa3f70b89dab
Sindre Nistad <snis@statoil.com>
2017-10-10 19:45:53 +0200

    feat: Added 'add facies', and extracted any hardcoded variables in the UI files

--- src/gui/wrappers/base_classes/dialogs.py
+++ src/gui/wrappers/base_classes/dialogs.py
@@ -2,12 +2,14 @@ from typing import Callable, Dict
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QKeyEvent
-from PyQt5.QtWidgets import QDialogButtonBox
+from PyQt5.QtWidgets import QDialogButtonBox, QDialog
 
-from src.utils.constants import Defaults
+from src.gui.wrappers.base_classes.getters.general import get_element
+from src.utils.constants import Defaults, AddFaciesElements
+from src.resources.ui.AddFacies_ui import Ui_AddFacies
 
 
-class GeneralDialog(QDialogButtonBox):
+class GeneralDialog(QDialogButtonBox, QDialog):
     def __init__(self, parent=None, name_of_buttons=Defaults.NAME_OF_BUTTON_BOX, **kwargs):
         # TODO: Make name_of_buttons into a constant
         super(GeneralDialog, self).__init__(parent=parent)
@@ -102,3 +104,26 @@ class OkCancelDialog(OkDialog):
         super(OkCancelDialog, self).keyPressEvent(event)
         if event.key() == Qt.Key_Escape:
             self.click_button(QDialogButtonBox.Cancel)
+
+
+class AddFacies(GeneralDialog, Ui_AddFacies):
+    def __init__(self, state, parent=None, name_of_buttons=Defaults.NAME_OF_BUTTON_BOX):
+        super(AddFacies, self).__init__(parent=parent, name_of_buttons=name_of_buttons)
+        self.setupUi(self)
+        self.retranslateUi(self)
+
+        self.wire_up(save=self.save, cancel=self.close)
+        self._state = state
+
+    def keyPressEvent(self, event: QKeyEvent):
+        key = event.key()
+        if key == Qt.Key_Return:
+            self.click_button(QDialogButtonBox.Save)
+        elif key == Qt.Key_Escape:
+            self.click_button(QDialogButtonBox.Cancel)
+
+    def save(self):
+        edit = get_element(self, AddFaciesElements.NEW_FACIES_NAME)
+        facies_name = edit.text()
+        self._state.add_facies(facies_name)
+        self.close()
--- src/gui/wrappers/main_window.py
+++ src/gui/wrappers/main_window.py
@@ -3,15 +3,16 @@
 A wrapper, and implementation of the main GUI window of the APS-GUI.
 'Implements' the design in APS_prototype.ui, and wraps around src/resources/ui/APS_prototype_ui.py.
 """
-from typing import Dict, Union
+from typing import Callable, Dict, Union
 
 from PyQt5.QtWidgets import *
 
-from src.gui.wrappers.define_gaussian import DefineGaussian
-from src.gui.wrappers.base_classes.getters.general import get_element
+from src.gui.wrappers.base_classes.dialogs import AddFacies
 from src.gui.state import State
 from src.gui.wrappers.assign_probabilities import AssignProbabilities
+from src.gui.wrappers.base_classes.getters.general import get_element, get_elements
 from src.gui.wrappers.base_classes.message_box import MessageBox
+from src.gui.wrappers.define_gaussian import DefineGaussian
 from src.gui.wrappers.truncation_rule import (
     BayfillTruncationRule, CubicTruncationRule, CustomTruncationRule,
     NonCubicTruncationRule,
@@ -19,8 +20,8 @@ from src.gui.wrappers.truncation_rule import (
 from src.resources.ui.APS_prototype_ui import Ui_MainWindow
 from src.utils.checks import is_valid_path
 from src.utils.constants import (
-    Defaults, ModeConstants, TruncationLibraryKeys, TruncationLibrarySubKeys,
-    TruncationRuleLibraryElements, GaussianRandomFieldElements,
+    Defaults, FaciesSelectionElements, GaussianRandomFieldElements, MainWindowElements,
+    ModeConstants, TruncationLibraryKeys, TruncationLibrarySubKeys, ZoneSelectionElements,
 )
 from src.utils.mappings import truncation_library_button_to_kind_and_number_of_facies, truncation_library_elements
 from src.utils.methods import get_project_file, toggle_elements
@@ -40,14 +41,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.wire_up()
         self.probabilities = None
         self.gaussian_settings = None
+        self.new_facies_dialog = None
 
     def wire_up(self):
-        self.m_button_assign_probability.clicked.connect(self.assign_probabilities)
-        self.m_button_close.clicked.connect(self.close)
-        self.m_toggle_seperate_zone_models.stateChanged.connect(self._toggle_separate_zone_models)
-        self.m_toggle_select_facies.stateChanged.connect(self._toggle_select_facies)
-        self.m_toggle_experimental_mode.clicked.connect(self._toggle_experimental_mode)
-        self.m_toggle_condition_to_wells.clicked.connect(self._condition_to_wells)
+        self.wire_up_navigation()
+        self.wire_up_selections()
 
         self.initialize_truncation_rules()
         self.initialize_project_data()
@@ -56,6 +54,28 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.initialize_gaussian_random_field_clickability()
 
         self._set_checkboxes_to_defaults()
+
+    def wire_up_selections(self):
+        buttons = {
+            ZoneSelectionElements.TOGGLE:   self._toggle_separate_zone_models,
+            FaciesSelectionElements.TOGGLE: self._toggle_select_facies,
+        }
+        self._connect_buttons_to_functions_on_event(buttons, 'stateChanged')
+
+    def _connect_buttons_to_functions_on_event(self, buttons: Dict[str, Callable], event: str):
+        for key, fun in buttons.items():
+            button = get_element(self, key)
+            button.__getattribute__(event).connect(fun)
+
+    def wire_up_navigation(self):
+        buttons = {
+            MainWindowElements.ASSIGN_PROBABILITY_CUBE: self.assign_probabilities,
+            MainWindowElements.CLOSE:                   self.close,
+            MainWindowElements.EXPERIMENTAL_MODE:       self._toggle_experimental_mode,
+            MainWindowElements.CONDITION_TO_WELL:       self._condition_to_wells,
+            FaciesSelectionElements.ADD_FACIES:         self.add_facies,
+        }
+        self._connect_buttons_to_functions_on_event(buttons, 'clicked')
 
     def initialize_gaussian_random_field_clickability(self):
         # TODO: Read how many facies are available, and make sure that no more than that can be clicked.
@@ -81,8 +101,13 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         if self._state:
             for key in self._state.keys():
                 if key == ModeConstants.EXECUTION_MODE and self._state.is_experimental_mode():
-                    self.m_toggle_experimental_mode.setChecked(True)
+                    check_box = get_element(self, MainWindowElements.EXPERIMENTAL_MODE)
+                    check_box.setChecked(True)
                 pass
+
+    def add_facies(self):
+        self.new_facies_dialog = AddFacies(self._state)
+        self.new_facies_dialog.show()
 
     def assign_probabilities(self, content_in_combo_boxes=None):
         # TODO: Read from file, and give that as input
@@ -139,30 +164,32 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         dialog.show()
 
     def _toggle_separate_zone_models(self):
-        toggled = self.m_toggle_seperate_zone_models.isChecked()
-        elements = [
-            self.label_available_zones, self.label_selected_zones, self.label_zones_to_be_modeled,
-            self.m_list_available_zones, self.m_list_selected_zones, self.m_button_add_zone, self.m_button_remove_zone
-        ]
-        toggle_elements(toggled, elements)
+        self._toggle_elements_in_selection_area(ZoneSelectionElements)
 
     def _toggle_select_facies(self):
-        toggled = self.m_toggle_select_facies.isChecked()
-        elements = [
-            self.label_available_facies, self.label_facies_to_be_modelled, self.label_selected_facies,
-            self.m_list_available_facies, self.m_list_selected_facies, self.m_button_add_facies,
-            self.m_button_remove_facies, self.m_button_add_new_facies, self.m_button_remove_selected,
-        ]
+        self._toggle_elements_in_selection_area(FaciesSelectionElements)
+
+    def _toggle_elements_in_selection_area(self, constants):
+        check_box = get_element(self, constants.TOGGLE)
+        toggled = check_box.isChecked()
+        elements = get_elements(self, constants.values(local=True))
+        elements.remove(check_box)
         toggle_elements(toggled, elements)
 
     def _set_checkboxes_to_defaults(self):
-        self.m_toggle_seperate_zone_models.setCheckState(Defaults.SEPARATE_ZONE_MODELS)
-        self.m_toggle_select_facies.setCheckState(Defaults.FACIES_MODELS)
+        buttons = {
+            ZoneSelectionElements.TOGGLE:   Defaults.SEPARATE_ZONE_MODELS,
+            FaciesSelectionElements.TOGGLE: Defaults.FACIES_MODELS,
+        }
+        for key, default in buttons.items():
+            check_box = get_element(self, key)
+            check_box.setCheckState(default)
         self._toggle_separate_zone_models()
         self._toggle_select_facies()
 
     def _toggle_experimental_mode(self):
-        use_experimental_mode = self.m_toggle_experimental_mode.isChecked()
+        experimental_check_box = get_element(self, MainWindowElements.EXPERIMENTAL_MODE)
+        use_experimental_mode = experimental_check_box.isChecked()
         if not use_experimental_mode and not self._state.has_valid_path():
             # The state is invalid, and the user must choose a project file
 
@@ -173,7 +200,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 self._state.set_project_path(path)
             else:
                 self._state.set_experimental_mode()
-                self.m_toggle_experimental_mode.setChecked(True)
+                experimental_check_box.setChecked(True)
         elif use_experimental_mode:
             # TODO:
             pass
