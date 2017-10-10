899eb10b208ac800124eb11e0d9b54a0a45cc575
Sindre Nistad <snis@statoil.com>
2017-10-10 16:18:40 +0200

    init: Use SQLite for handeling the state of the GUI

--- src/gui/models.py
+++ src/gui/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from peewee import *
 from playhouse.sqlite_ext import JSONField, SqliteExtDatabase
 
@@ -9,3 +10,28 @@ db = SqliteExtDatabase(DataBase.NAME)
 class BaseModel(Model):
     class Meta:
         database = db
+
+
+class MetaData(BaseModel):
+    key = TextField(unique=True, index=True, null=False)
+    value = TextField(unique=False, null=True)  # TODO: Is a textfield sufficient?
+
+
+class TimeStamp(BaseModel):
+    time_stamp = DateTimeField(default=datetime.now())
+
+
+class Zone(BaseModel):
+    pass
+
+
+class Region(BaseModel):
+    pass
+
+
+class Facies(BaseModel):
+    pass
+
+
+class GaussianRandomField(BaseModel):
+    pass
