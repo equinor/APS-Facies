cc00d2e3838a0731bf3b4dcb2fd98ae4a0f46651
Sindre Nistad <snis@statoil.com>
2017-10-16 18:05:44 +0200

    feat: Added test to make sure that the GUI is able to run, and added some commandline options

--- src/gui/app.py
+++ src/gui/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import sys
-
+import argparse
 from PyQt5.QtWidgets import QApplication
 
 from src.gui.state import State
@@ -9,6 +9,20 @@ from src.gui.wrappers.project import Project
 from src.gui.wrappers.truncation_rule import CubicTruncationRule, NonCubicTruncationRule, BayfillTruncationRule
 from src.gui.wrappers.main_window import MainWindow
 from src.gui.wrappers.define_gaussian import DefineGaussian
+
+parser = argparse.ArgumentParser(
+    prog="APS-GUI",
+    description="A GUI for setting parameters to plurigaussian simulations."
+)
+parser.add_argument('--project', help="Starts the GUI with the given project file")
+parser.add_argument('--version', action='version', version='%(prog)s 0.0.1')
+parser.add_argument(
+    '--verbosity',
+    help="Sets the verbosity level. 0 is off, while 4 is the highest",
+    type=int,
+    choices=[0, 1, 2, 3, 4]
+)
+args = parser.parse_args()
 
 
 def run():
