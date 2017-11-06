7021e35fb2a77cb96e3ad5324e44fead1b735440
Sindre Nistad <snis@statoil.com>
2017-11-06 10:09:02 +0000

    feat: Added example files for APS.xml and rms project
    feat:

--- src/utils/constants/autogenerated.py
+++ src/utils/constants/autogenerated.py
@@ -1,7 +1,16 @@
 from enum import Enum
 
 
+VERSION_HASH = ''  # Set automatically
+
+
 class DrawingLibrary(Enum):
-    LIBRARY_FOLDER = '/home/aps-gui/APS-GUI/libraries'  # Set automatically by make
+    LIBRARY_FOLDER = ''  # Set automatically by make
     NAME = 'libdraw2D.so'
     LIBRARY_PATH = LIBRARY_FOLDER + '/' + NAME
+
+
+class ExampleFiles(Enum):
+    EXAMPLE_FOLDER = '/home/aps-gui/APS-GUI/examples'  # Set automatically by make
+    MODEL = EXAMPLE_FOLDER + '/APS.xml'
+    RMS = EXAMPLE_FOLDER + '/rms_project_data_for_APS_gui.xml'
