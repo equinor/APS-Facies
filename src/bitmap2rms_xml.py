ed8e3c7b3b34c5315883dc079eabc1950ff5ef01
Oddvar Lia <olia@statoil.com>
2018-03-02 23:36:24 +0100

    Add utility script bitmap2rms_xml.py with example and shell script bitmap2rms.sh to run it with RMS python version

---
+++ src/bitmap2rms_xml.py
@@ -0,0 +1,110 @@
+#!/bin/env python
+# -------------------------------------------------------------------------------------
+# python3 script which can be run as a python job in RMS10 
+# Purpose: Read a rectangular piece of a bitmap color file with 1byte colors (256 colors) and information 
+#          about coordinates for the selected rectangle of the bitmap image and other 
+#          information to be able to convert the bitmap image into 
+#          an irap map format file that can be imported into RMS.
+# Input:   Bitmap input file (1byte) colors (256 colors at maximum)
+#          Parameter file containing reference to coordinates of the rectangular area
+#          to be selected as well as which facies belongs to which colour number.
+#          Note that the selected rectangular area of the bitmap is specified with its corner point coordinates
+#          and pixel numbers.
+# Output:  An irap map file containing the colour numbers for each pixel 
+#          or facies number for each pixel.
+#
+# Usage:   
+#          1. Import the script into an empty python job in e.g.a an empty rms workflow.
+#             The rms10 python version and environment contains all python modules
+#             necessary to run the script or alternatively run the script from a shell script 
+#             where the necessary environment varibles are set automatically.
+#          2. Update the input model file to fit your case.
+#          
+#  Description of input parameter file which is a xml file:
+#  Example:
+#<?xml version="1.0" ?>
+#<ConvertBitmapToRMS>
+#  <Coordinates>
+#    <xmin>   480000  </xmin>
+#    <xmax>   493000  </xmax>
+#    <ymin>   6688000 </ymin>
+#    <ymax>   6710000 </ymax>
+#  </Coordinates>
+#  <PixelInterval>
+#    <nx> 476 </nx>
+#    <ny>  841 </ny>
+#    <I> 18 468 </I>  
+#    <J> 9 769 </J>  
+#  </PixelInterval>
+#  <CropToPixelInterval> 1 </CropToPixelInterval> 
+#  <MissingCode> 9999900.000 </MissingCode>
+#  <ColorCode facies="1"> 168 </ColorCode>
+#  <ColorCode facies="2"> 157 </ColorCode>
+#  <ColorCode facies="3"> 232 </ColorCode>
+#  <ColorCode facies="4"> 111 </ColorCode>
+#  <ColorCode facies="5"> 1 </ColorCode>
+#  <Files>
+#    <Input> CorelDraw_aboveFS36_model_input.bmp </Input>
+#    <Output> out1.irap </Output>
+#  </Files>
+#  <Files>
+#    <Input> CorelDraw_aboveFS38_model_input.bmp </Input>
+#    <Output> out2.irap </Output>
+#  </Files>
+#</ConvertBitmapToRMS>
+#
+#  Explanation of keywords:
+#       Coordinates   -  specify coordinates for corner points of a rectangle in the map. (xmin,ymin) corresponds to pixel (Imin,Jmin)
+#                        (xmax,ymax) corresponds to pixel (Imax,Jmax)  where Imin,Imax,Jmin,Jmax is specified in keyword PixelInterval.
+#       PixelInterval -  specify number of pixels in the whole image and an 
+#                        interval of pixels corresponding to the rectangle defined by the cornerpoints. 
+#                        Hence the user will have to identify the corner point pixels (Imin,Jmin), (Imax,Jmax) and their corresponding
+#                        geographic (UTM coordinates) (xmin,ymin) and (xmax,ymax).
+#       ColorCode     -  specify which facies code corresponds to which color. The effect of this keyword is turned on/off by a 
+#                        command line value (0/1). The user will usually have to run without activating this keyword by using 
+#                        command line value 0 first to get a map with colour values. Find corresponding colour values and facies
+#                        and then specify that using this keyword before running a second time. Missing code (usually 9999900.000) is 
+#                        specified in separate keyword.
+#       CropToPixelInterval - Is set to 1 if the output maps should only contain the pixels in the specified rectangle. If it is set to 0
+#                             the script will calculate the geographic coordinates of pixel (1,1) and (nx,ny) and write out uncropped file
+#                             which also will contain area outside the specified rectangle.
+#       File          - Specify the input and output files.
+#
+#       NOTE: It is important to note that xmin,ymin,xmax,ymax define coordinates for the pixels specified by the PixelInterval keyword.              
+# --------------------------------------------------------------------------------------
+import xml.etree.ElementTree as ET
+from PIL import Image 
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+import matplotlib
+import sys
+import copy
+import ConvertBitMapToRMS
+
+# -------------  Main ----------------------
+if __name__ == "__main__":
+    modelFileName = 'bitmap2rms_model.xml'
+    defineFaciesCode = 0
+    if len(sys.argv) >= 2:
+        defineFaciesCode = int(sys.argv[1])
+        print('Calculate map with facies')
+
+    functionName = 'bitmap2rms'
+    testScript = 0
+
+
+convertBitmap = ConvertBitMapToRMS.ConvertBitMapToRMS(modelFileName,defineFaciesCode)
+
+convertBitmap.printContents()
+
+convertBitmap.convert()
+convertBitmap.writeFile()
+
+print('Finished: ' + functionName)
+
+if testScript == 0:
+    sys.exit()
+
+# ----------- This code is used for testing --------------------
+convertBitmap.testPlot()
