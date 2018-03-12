405c837c864edbe227e355c135e4ff1229bc7118
Sindre Nistad <snis@statoil.com>
2018-03-12 15:15:43 +0100

    refactor: Migrated requirements.txt to Pipfile

---
+++ bin/find-version-of-docker-image.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# A Utility that extracts the version from a Dockerfile's label
+# Written by Sindre Nistad, snis@statoil.com
+# Usage: ./find-version-of-docker-image.py [folder in which the Docker file is]
+
+from sys import argv
+from os.path import abspath
+
+
+def get_root_path():
+    if len(argv) == 1:
+        root_path = abspath('.')
+    else:
+        root_path = argv[1]
+        if root_path[-1] == '/':
+            root_path = root_path[:-1]
+    return root_path + '/Dockerfile'
+
+
+def get_docker_image_version(dockerfile):
+    with open(dockerfile, 'r') as f:
+        for line in f:
+            words = line.split(' ')
+            if words and words[0].lower() == 'label':
+                label = words[1]
+                version = label.lstrip('version=').strip('"').strip("'")
+                return version
+    return ""
+
+
+def run():
+    dockerfile = get_root_path()
+    docker_image_version = get_docker_image_version(dockerfile)
+    print(docker_image_version)
+
+
+if __name__ == '__main__':
+    run()
