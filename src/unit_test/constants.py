4dc5ca46568bb439c1e86fdc47332d681893b92a
Sindre Nistad <snis@statoil.com>
2018-05-23 16:19:09 +0200

    fix: Ensure tests can be run from a normal pyton /pytest run from src

--- src/unit_test/constants.py
+++ src/unit_test/constants.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from src.utils.constants.simple import Debug
 
 RMS_PROJECT = 'testNeslen.rms10'
--- src/utils/io.py
+++ src/utils/io.py
@@ -1,3 +1,5 @@
+from os.path import exists
+
 import numpy as np
 
 from src.utils.constants.simple import Debug
@@ -6,10 +8,11 @@ from src.utils.constants.simple import Debug
 def writeFile(fileName, a, nx, ny, debug_level=Debug.OFF):
     with open(fileName, 'w') as file:
         # Choose an arbitrary heading
-        outstring = '-996  ' + str(ny) + '  50.000000     50.000000\n'
-        outstring += '637943.187500   678043.187500  4334008.000000  4375108.000000\n'
-        outstring += ' ' + str(nx) + ' ' + ' 0.000000   637943.187500  4334008.000000\n'
-        outstring += '0     0     0     0     0     0     0\n'
+        outstring = '''-996  {ny}  50.000000     50.000000
+637943.187500   678043.187500  4334008.000000  4375108.000000
+ {nx}  0.000000   637943.187500  4334008.000000
+0     0     0     0     0     0     0
+'''.format(ny=ny, nx=nx)
         count = 0
         text = ''
         if debug_level >= Debug.SOMEWHAT_VERBOSE:
@@ -25,11 +28,16 @@ def writeFile(fileName, a, nx, ny, debug_level=Debug.OFF):
         if count > 0:
             outstring += text + '\n'
         file.write(outstring)
-    print('Write file: ' + fileName)
+    if debug_level >= Debug.SOMEWHAT_VERBOSE:
+        print('Write file: ' + fileName)
 
 
 def readFile(fileName, debug_level=Debug.OFF):
-    print('Read file: ' + fileName)
+    if debug_level >= Debug.SOMEWHAT_VERBOSE:
+        print('Read file: ' + fileName)
+    if not exists(fileName):
+        file_location = 'src/unit_test/'
+        fileName = file_location + fileName
     with open(fileName, 'r') as file:
         inString = file.read()
         words = inString.split()
@@ -45,12 +53,13 @@ def readFile(fileName, debug_level=Debug.OFF):
             a[i - 19] = float(words[i])
     return a, nx, ny
 
+
 def writeFileRTF(fileName, a, nx, ny, dx, dy, x0, y0, debug_level=Debug.OFF):
     # Write in Roxar text format
     with open(fileName, 'w') as file:
-        outstring = '-996  ' + str(ny) + '  ' + str(dx) + ' ' + str(dy) +'\n'
-        outstring += str(x0) + ' ' + str(x0 + nx*dx) + ' ' + str(y0) + ' ' + str(y0+ny*dy) +'\n'
-        outstring += ' ' + str(nx) + ' ' + ' 0.000000  ' +  str(x0) + ' ' + str(y0) + '\n'
+        outstring = '-996  ' + str(ny) + '  ' + str(dx) + ' ' + str(dy) + '\n'
+        outstring += str(x0) + ' ' + str(x0 + nx*dx) + ' ' + str(y0) + ' ' + str(y0+ny*dy) + '\n'
+        outstring += ' ' + str(nx) + ' ' + ' 0.000000  ' + str(x0) + ' ' + str(y0) + '\n'
         outstring += '0     0     0     0     0     0     0\n'
         count = 0
         text = ''
@@ -67,5 +76,6 @@ def writeFileRTF(fileName, a, nx, ny, dx, dy, x0, y0, debug_level=Debug.OFF):
         if count > 0:
             outstring += text + '\n'
         file.write(outstring)
-    print('Write file: ' + fileName)
+    if debug_level >= Debug.SOMEWHAT_VERBOSE:
+        print('Write file: ' + fileName)
 
