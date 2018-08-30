6c0f97b341ca4dc28b1458f8ee18a4781a2e22ee
Sindre Nistad <snis@statoil.com>
2018-08-30 09:32:35 +0200

    feat: Made the (entire) GRF panel expandable

--- src/gui/src/components/specification/GaussianRandomField/multiple.vue
+++ src/gui/src/components/specification/GaussianRandomField/multiple.vue
@@ -1,65 +1,74 @@
 <template>
-  <div>
-    <v-layout
-      justify-center
-      align-center
-      row
-      pa-0
-      ma-0
-    >
-      <v-flex>
-        <h2>Gaussian Random Fields (GRF)</h2>
-      </v-flex>
-      <v-flex xs2>
-        <v-btn
-          icon
-          @click="addField"
+  <v-expansion-panel
+    v-model="expanded"
+  >
+    <v-expansion-panel-content>
+      <div slot="header">
+        <v-layout
+          justify-center
+          align-center
+          row
+          pa-0
+          ma-0
         >
-          <v-icon>add_box</v-icon>
-        </v-btn>
-      </v-flex>
-    </v-layout>
-    <v-expansion-panel>
-      <v-expansion-panel-content
-        v-for="grfId in ids"
-        :key="grfId"
-      >
-        <div slot="header">
-          <v-layout
-            align-center
-            justify-start
-            row
-            fill-height
-            wrap
+          <v-flex>
+            <h2>Gaussian Random Fields (GRF)</h2>
+          </v-flex>
+          <v-flex
+            v-show="isOpen"
+            xs2
           >
-            <v-flex xs2>
-              <gaussian-field-name
-                :ref="grfId"
-                :grf-id="grfId"
-              />
-            </v-flex>
-            <v-flex
-              xs1
+            <v-btn
+              icon
+              @click.stop="addField"
             >
-              <v-btn
-                flat
-                icon
-                @click.stop="deleteField(grfId)"
+              <v-icon>add_box</v-icon>
+            </v-btn>
+          </v-flex>
+        </v-layout>
+      </div>
+      <v-expansion-panel>
+        <v-expansion-panel-content
+          v-for="grfId in ids"
+          :key="grfId"
+        >
+          <div slot="header">
+            <v-layout
+              align-center
+              justify-start
+              row
+              fill-height
+              wrap
+            >
+              <v-flex xs2>
+                <gaussian-field-name
+                  :ref="grfId"
+                  :grf-id="grfId"
+                />
+              </v-flex>
+              <v-flex
+                xs1
               >
-                <v-icon>delete</v-icon>
-              </v-btn>
-              <confirmation-dialog :ref="`confirmation_${grfId}`"/>
-            </v-flex>
-          </v-layout>
-        </div>
-        <v-card>
-          <gaussian-random-field
-            :grf-id="grfId"
-          />
-        </v-card>
-      </v-expansion-panel-content>
-    </v-expansion-panel>
-  </div>
+                <v-btn
+                  flat
+                  icon
+                  @click.stop="deleteField(grfId)"
+                >
+                  <v-icon>delete</v-icon>
+                </v-btn>
+                <confirmation-dialog :ref="`confirmation_${grfId}`"/>
+              </v-flex>
+            </v-layout>
+          </div>
+          <v-card>
+            <gaussian-random-field
+              :grf-id="grfId"
+            />
+          </v-card>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
 </template>
 
 <script>
@@ -74,7 +83,14 @@ export default {
     GaussianRandomField,
   },
 
+  data () {
+    return {
+      expanded: null,
+    }
+  },
+
   computed: {
+    isOpen () { return this.expanded === 0 },
     fields () { return this.$store.state.gaussianRandomFields.fields },
     ids () { return Object.keys(this.fields) },
   },
