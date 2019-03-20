b9e96d7e5e20792c8c76b9392ead0c4c4b66acab
Sindre Nistad <snis@statoil.com>
2019-03-20 13:52:39 +0100

    chore: Added `Test_defineFaciesProbTrend`, as it now works. Closes #60

--- src/unit_test/integration/test_workflows_in_rms11.sh
+++ src/unit_test/integration/test_workflows_in_rms11.sh
@@ -7,24 +7,22 @@ RMS_PROJECT="testAPSWorkflow_new.rms11"
 fi
 if [ -z "${RMS}" ]; then
     if [ -z "${RMS_VERSION}" ]; then
-        RMS_VERSION=11.0.0
+        RMS_VERSION=11.0.1
     fi
 RMS="/prog/roxar/site/RMS${RMS_VERSION}/rms/rms"
 fi
 
-# Test_APS_deterministic_interpretation
 # Example_APS
-# De andre kan sjekkes manuelt, dvs
+# The rest can be check manually, dvs
 #   Test_Update_FMU_parameters
 #   Test_APS_uncertainty_workflow
-#   Test_defineFaciesProbTrend
 #   Example_APS_workflow_multi_process
 
-# TODO: Make sure the workflow "Test_defineFaciesProbTrend" works as intended
 for workflow in "Example_APS_Coarse_unconditioned" \
                 "Example_APS_Coarse_conditioned" \
                 "Example_APS_Coarse_uncond_regions" \
                 "Example_APS_Fine_unconditioned" \
+                "Test_defineFaciesProbTrend" \
                 "Test_Update_FMU_parameters" \
                 "Test_APS_deterministic_interpretation" \
                 "Test_APS_uncertainty_workflow"
