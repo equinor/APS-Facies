0dbf0f5cbacd1923be214a0368fd12557b31e3f8
Sindre Nistad <snis@statoil.com>
2019-05-06 10:08:12 +0200

    feat: Help icon with hover text

---
+++ src/gui/src/components/baseComponents/HelpIcon.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-popover
+    trigger="hover"
+  >
+    <v-icon
+      :color="color"
+    >
+      {{ $vuetify.icons.help }}
+    </v-icon>
+    <span slot="popover">
+      <slot />
+    </span>
+  </v-popover>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+import { Color } from '@/utils/domain/facies/helpers/colors'
+
+@Component
+export default class HelpIcon extends Vue {
+  @Prop({ default: 'black' })
+  readonly color: Color
+}
+</script>
