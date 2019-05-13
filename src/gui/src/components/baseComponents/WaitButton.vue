93e1d28748c1715ccec109124fd1fb33135c87b0
Sindre Nistad <snis@statoil.com>
2019-05-13 12:53:51 +0200

    feat: Added popover to WaitButton

--- src/gui/src/components/baseComponents/WaitButton.vue
+++ src/gui/src/components/baseComponents/WaitButton.vue
@@ -1,21 +1,32 @@
 <template>
-  <v-btn
-    :disabled="waiting || disabled"
-    :dark="dark"
-    :flat="flat"
-    :color="color"
-    @click="e => $emit('click', e)"
+  <v-popover
+    :disabled="!tooltipText"
+    trigger="hover"
   >
-    <slot
-      v-if="!title && !waiting"
-    />
-    <span v-if="!waiting">
-      {{ title }}
+    <v-btn
+      :disabled="waiting || disabled"
+      :dark="dark"
+      :flat="flat"
+      :color="color"
+      :outline="outline"
+      @click="e => $emit('click', e)"
+    >
+      <slot
+        v-if="!title && !waiting"
+      />
+      <span v-if="!waiting">
+        {{ title }}
+      </span>
+      <span v-else>
+        <v-progress-circular indeterminate />
+      </span>
+    </v-btn>
+    <span
+      slot="popover"
+    >
+      {{ tooltipText }}
     </span>
-    <span v-else>
-      <v-progress-circular indeterminate />
-    </span>
-  </v-btn>
+  </v-popover>
 </template>
 
 <script lang="ts">
@@ -27,11 +38,17 @@ export default class WaitButton extends Vue {
   @Prop({ default: '' })
   readonly title!: string
 
+  @Prop({ default: '' })
+  readonly tooltipText: string
+
   @Prop({ default: false })
   readonly waiting: boolean
 
   @Prop({ default: false })
   readonly disabled: boolean
+
+  @Prop({ default: false })
+  readonly outline: boolean
 
   @Prop({ default: false })
   readonly flat: boolean
