c16274392465b6e2d0b295b9dc3e74000afa7a10
Sindre Nistad <snis@statoil.com>
2019-05-14 13:01:14 +0200

    feat: Select the given grid model from RMS

--- src/gui/src/plugins/rms.js
+++ src/gui/src/plugins/rms.js
@@ -12,6 +12,18 @@ if (typeof rms !== 'undefined') {
     // TODO: Handle different versions, (and merge?)
     if (Object.keys(data).length > 2) {
       store.dispatch('populate', data)
+    } else {
+      let gridModel = /^Grid models\/(.*)$/g.exec(data['_treeorigin'])[1]
+      // The resulting output may include a nested path (/-separated), while a grid model MAY have the '/' character
+      for (const model of store.state.gridModels.available) {
+        if (gridModel.includes(model)) {
+          gridModel = model
+          break
+        }
+      }
+      if (gridModel) {
+        store.dispatch('gridModels/select', gridModel)
+      }
     }
   })
 }
