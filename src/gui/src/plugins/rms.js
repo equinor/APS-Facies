f439ab45f5f14c89d5537372544e6b98479e8d43
Sindre Nistad <snis@statoil.com>
2019-05-22 13:20:59 +0200

    fix: Gracefully deals with no match

--- src/gui/src/plugins/rms.js
+++ src/gui/src/plugins/rms.js
@@ -13,16 +13,19 @@ if (typeof rms !== 'undefined') {
     if (Object.keys(data).length > 2) {
       store.dispatch('populate', data)
     } else {
-      let gridModel = /^Grid models\/(.*)$/g.exec(data['_treeorigin'])[1]
-      // The resulting output may include a nested path (/-separated), while a grid model MAY have the '/' character
-      for (const model of store.state.gridModels.available) {
-        if (gridModel.includes(model)) {
-          gridModel = model
-          break
+      const match = /^Grid models\/(.*)$/g.exec(data['_treeorigin'])
+      if (match) {
+        let gridModel = match[1]
+        // The resulting output may include a nested path (/-separated), while a grid model MAY have the '/' character
+        for (const model of store.state.gridModels.available) {
+          if (gridModel.includes(model)) {
+            gridModel = model
+            break
+          }
         }
-      }
-      if (gridModel) {
-        store.dispatch('gridModels/select', gridModel)
+        if (gridModel) {
+          store.dispatch('gridModels/select', gridModel)
+        }
       }
     }
   })
