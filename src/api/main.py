401e913ac8713cbba96b8d06828dcd7da67300dc
Sindre Nistad <snis@equinor.com>
2019-11-21 10:10:25 +0100

    refactor: Use kwargs

--- src/api/main.py
+++ src/api/main.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 
 from src.algorithms.APSModel import APSModel
-from src.rms_jobs.APS_normalize_prob_cubes import run as run_normalization
-from src.rms_jobs.update_trend_location_relative_to_fmu import run as run_update_trend_location
-from src.rms_jobs.APS_simulate_gauss_singleprocessing import run as run_simulation
 from src.rms_jobs.APS_main import run as run_truncation
+from src.rms_jobs.APS_normalize_prob_cubes import run as run_normalization
+from src.rms_jobs.APS_simulate_gauss_singleprocessing import run as run_simulation
 from src.rms_jobs.updateAPSModelFromFMU import run as run_update_fmu_variables_in_model_file
+from src.rms_jobs.update_trend_location_relative_to_fmu import run as run_update_trend_location
 from src.utils.io import create_temporary_model_file
 
 import roxar
@@ -54,47 +54,35 @@ def run(config):
     if config.error_message:
         raise ValueError(config.error_message)
     with create_temporary_model_file(config.model) as model_file:
+        kwargs = {
+            'roxar': roxar,
+            'project': project,
+            'model_file': model_file,
+            'output_model_file': model_file,
+            'global_include_file': config.global_include_file,
+            'max_fmu_grid_depth': config.max_fmu_grid_depth,
+            'layers_per_zone': [
+                config.max_fmu_grid_depth for _ in range(len(project.zones))
+            ] if config.run_fmu_workflows else None,
+            'fmu_mode': config.run_fmu_workflows,
+            'seed_log_file': None,
+            'write_rms_parameters_for_qc_purpose': False,
+        }
         use_constant_probabilities = APSModel(model_file).use_constant_probability
         if not use_constant_probabilities:
-            run_normalization(roxar, project, model_file=model_file)
+            run_normalization(**kwargs)
         if config.run_fmu_workflows:
             # Add a flag for whether `run_initial_ensable`, or `run_import_from_fmu` should be run
             # (user specified in the GUI)
             # We also need to get / update the truncation rule parameters (in the model file) that FMU may change
             # That is, to use a different fmu_variables file (similar / equivalent to global_variables.ipl)
-            run_update_fmu_variables_in_model_file(
-                roxar, project,
-                model_file=model_file,
-                output_model_file=model_file,
-                global_include_file=config.global_include_file,
-            )
+            run_update_fmu_variables_in_model_file(**kwargs)
             # run_initial_ensable is equivalent to running the APS workflows ONCE
             # Once that is done, (that is the the facies realization, and GRFs with trends exists),
             # only loading from disk, and running `run_truncation` should be done
-
-        kwargs = {}
-        if config.run_fmu_workflows:
-            run_update_trend_location(
-                roxar, project,
-                model_file=model_file,
-                max_fmu_grid_depth=config.max_fmu_grid_depth,
-            )
-            kwargs = {
-                'layers_per_zone': [config.max_fmu_grid_depth for _ in range(len(project.zones))],
-            }
-        run_simulation(
-            roxar, project,
-            model_file=model_file,
-            seed_log_file=None,
-            fmu_mode=config.run_fmu_workflows,
-            write_rms_parameters_for_qc_purpose=False,
-            **kwargs,
-        )
+            run_update_trend_location(**kwargs)
+        run_simulation(**kwargs)
         # The GRFs should not be written to RMS if Debug is not set, and we are not in FMU mode
 
         # Script for moving stuff from "FMU" grid to "regular" grid
-        run_truncation(
-            roxar, project,
-            model_file=model_file,
-            write_rms_parameters_for_qc_purpose=False,
-        )
+        run_truncation(**kwargs)
