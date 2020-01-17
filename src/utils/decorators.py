c6ea6db1395580552c73f020d1e4f51292bdb4a0
Sindre Nistad <snis@equinor.com>
2020-01-17 17:42:06 +0100

    fix: Writes debug information to disk properly

--- src/utils/decorators.py
+++ src/utils/decorators.py
@@ -2,9 +2,12 @@ import json
 import random
 import string
 import time
+import traceback
 from base64 import b64decode
 from functools import wraps
-from zipfile import ZipFile, ZIP_LZMA
+from pathlib import Path
+from typing import Union
+from zipfile import ZipFile, ZIP_DEFLATED
 
 from src.utils.constants.simple import Debug
 
@@ -42,11 +45,17 @@ def loggable(func):
                     random=''.join(random.choices(string.ascii_letters + string.digits, k=5)),
                 )
                 model = config['model']
-                with ZipFile(prefix + '.zip', mode='x', compression=ZIP_LZMA) as zipfile:
-                    with zipfile.open('model.xml', 'w') as f:
-                        f.write(b64decode(model))
-                    with zipfile.open('state.json', 'w') as f:
-                        json.dump(config, f)
+                with ZipFile(prefix + '.zip', mode='x', compression=ZIP_DEFLATED) as zipfile:
+                    def dump(file_name, data: Union[str, bytes], encoding='ascii'):
+                        path = Path(prefix) / file_name
+                        with zipfile.open(str(path), 'w') as f:
+                            if isinstance(data, str):
+                                data = data.encode(encoding)
+                            f.write(data)
+
+                    dump('model.xml', b64decode(model))
+                    dump('state.json', json.dumps(config))
+                    dump('traceback.txt', traceback.format_exc())
             raise e
 
     return wrapper
