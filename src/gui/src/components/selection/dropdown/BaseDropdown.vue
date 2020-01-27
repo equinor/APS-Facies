829004373c1320ccfe436e670972a3938e66687b
Sindre Nistad <snis@equinor.com>
2020-01-27 14:01:15 +0100

    refactor: Warn user when selecting a new grid model

--- src/gui/src/components/selection/dropdown/BaseDropdown.vue
+++ src/gui/src/components/selection/dropdown/BaseDropdown.vue
@@ -1,37 +1,72 @@
 <template>
-  <v-select
-    v-model="selected"
-    :items="items"
-    :label="label"
-    :disabled="disabled"
-    :no-data-text="noDataText"
-  />
+  <div>
+    <confirmation-dialog
+      v-if="warn"
+      ref="confirm"
+    />
+    <v-select
+      ref="selection"
+      v-model.lazy="selected"
+      :items="items"
+      :disabled="disabled"
+      :label="label"
+    />
+  </div>
 </template>
 
 <script lang="ts">
+/* eslint-disable @typescript-eslint/ban-ts-ignore */
 import { Component, Prop, Vue } from 'vue-property-decorator'
 
-@Component
+import ConfirmationDialog from '@/components/specification/GaussianRandomField/ConfirmationDialog.vue'
+
+import { ListItem } from '@/utils/typing'
+
+@Component({
+  components: {
+    ConfirmationDialog,
+  },
+})
 export default class BaseDropdown<T> extends Vue {
+  @Prop({ required: true })
+  readonly value!: T
+
   @Prop({ required: true })
   readonly label!: string
 
   @Prop({ required: true })
-  readonly items!: string[]
-
-  @Prop({ required: true })
-  readonly modelGetter!: () => T
-
-  @Prop({ required: true })
-  readonly modelSetter!: (value: T) => void
+  readonly items!: ListItem<T>[] | T[]
 
   @Prop({ default: false })
   readonly disabled!: boolean
 
-  @Prop({ default: '$vuetify.noDataText' })
-  readonly noDataText!: string
+  @Prop({ default: false, type: Boolean })
+  readonly warn!: boolean
 
-  get selected (): T { return this.modelGetter() }
-  set selected (value: T) { this.modelSetter(value) }
+  @Prop({ default: '' })
+  readonly warnMessage!: string
+
+  @Prop({ default: false, type: Boolean })
+  readonly warnEvenWhenEmpty: boolean
+
+  get selected (): T { return this.value }
+  set selected (value: T) {
+    const changeValue = (): void => { this.$emit('input', value) }
+
+    if (this.warn && (!!this.selected || this.warnEvenWhenEmpty)) {
+      // @ts-ignore
+      (this.$refs.confirm as ConfirmationDialog).open('Are you sure?', this.warnMessage)
+        .then((confirmed: boolean) => {
+          if (confirmed) changeValue()
+          else {
+            // The component must be made aware that its value was not updated
+            // @ts-ignore
+            this.$refs.selection.lazyValue = this.selected
+          }
+        })
+    } else {
+      changeValue()
+    }
+  }
 }
 </script>
