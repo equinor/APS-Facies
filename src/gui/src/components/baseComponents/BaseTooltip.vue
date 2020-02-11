79f57818142212440bf70740d205c7be272e9591
Sindre Nistad <snis@equinor.com>
2020-02-11 17:12:40 +0100

    feat: Warn user about incompatibility between ERT-mode, and regions
    Closes: APS/GUI#274

---
+++ src/gui/src/components/baseComponents/BaseTooltip.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-popover
+    trigger="hover"
+    :disabled="!_message || disabled"
+  >
+    <slot />
+    <span slot="popover">{{ _message }}</span>
+  </v-popover>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component({
+})
+export default class BaseTooltip extends Vue {
+  @Prop({ default: '' })
+  readonly message!: string
+
+  @Prop({ default: false, type: Boolean })
+  readonly disabled!: boolean
+
+  get _message (): string | undefined { return this.message || undefined }
+}
+</script>
