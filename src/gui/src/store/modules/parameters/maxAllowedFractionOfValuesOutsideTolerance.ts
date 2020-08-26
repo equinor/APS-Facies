ee35bc08d0f7168e4da0761d5c380856c617193d
Sindre Nistad <snis@equinor.com>
2020-08-26 16:02:41 +0200

    refactor: Add method for creating module

--- src/gui/src/store/modules/parameters/maxAllowedFractionOfValuesOutsideTolerance.ts
+++ src/gui/src/store/modules/parameters/maxAllowedFractionOfValuesOutsideTolerance.ts
@@ -1,31 +1,5 @@
-import rms from '@/api/rms'
-import { Selectable } from '@/store/modules/parameters/typing/helpers'
-import { Module } from 'vuex'
-import { RootState } from '@/store/typing'
+import { makeToleranceModule } from '@/store/modules/parameters/utils'
 
-const module: Module<Selectable<number>, RootState> = {
-  namespaced: true,
-
-  state: {
-    selected: 0,
-  },
-
-  actions: {
-    async fetch ({ dispatch }): Promise<void> {
-      const { tolerance } = await rms.constants('max_allowed_fraction_of_values_outside_tolerance', 'tolerance')
-      await dispatch('select', tolerance)
-    },
-
-    async select ({ commit }, value: number): Promise<void> {
-      commit('SET', value)
-    }
-  },
-
-  mutations: {
-    SET (state, value: number): void {
-      state.selected = value
-    },
-  },
-}
+const module = makeToleranceModule('max_allowed_fraction_of_values_outside_tolerance')
 
 export default module
