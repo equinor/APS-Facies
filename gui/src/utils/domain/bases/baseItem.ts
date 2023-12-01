55b579271121c551c67912165c13381f30886078
Sindre Nistad <snis@equinor.com>
2023-12-01 20:04:49 +0100

    chore: Remove unused method

--- gui/src/utils/domain/bases/baseItem.ts
+++ gui/src/utils/domain/bases/baseItem.ts
@@ -17,7 +17,6 @@ export default class BaseItem implements Identifiable {
   public readonly id: ID
   protected readonly _excludeFromHash: string[]
 
-  // eslint-disable-next-line no-use-before-define
   public constructor({ id }: BaseItemConfiguration = { id: undefined }) {
     if (!id) id = uuid()
     if (!isUUID(id)) throw TypeError('An item must have a valid UUID, as id')
@@ -39,28 +38,6 @@ export default class BaseItem implements Identifiable {
     return hash(this._hashify(), {
       excludeKeys: (key): boolean => this._excludeFromHash.includes(key),
     })
-  }
-
-  protected objectify(): { [_: string]: any } {
-    // Include all (computed) properties, while dumping state to JSON
-    // This makes reconstruction / population _much_ easier
-    // Adapted from https://stackoverflow.com/a/50785428, and https://stackoverflow.com/a/8024294
-    const jsonObj = Object.assign({}, this)
-    // eslint-disable-next-line @typescript-eslint/no-this-alias
-    let proto = this
-    do {
-      for (const key of Object.getOwnPropertyNames(proto)) {
-        if (/__.*__/.exec(key)) continue
-        const desc = Object.getOwnPropertyDescriptor(proto, key)
-        const hasGetter = desc && typeof desc.get === 'function'
-        if (hasGetter) {
-          jsonObj[key] = this[key]
-        }
-      }
-      // eslint-disable-next-line no-cond-assign
-    } while ((proto = Object.getPrototypeOf(proto)))
-
-    return jsonObj
   }
 }
 
