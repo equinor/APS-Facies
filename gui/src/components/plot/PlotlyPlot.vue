f855fe9d389ff3407bdc14224503754b76cb584b
Sindre Nistad <snis@equinor.com>
2023-12-07 15:41:56 +0100

    fix: Always replot when layout changes
    We are using the layout to show SVG as if it was data

--- gui/src/components/plot/PlotlyPlot.vue
+++ gui/src/components/plot/PlotlyPlot.vue
@@ -120,7 +120,7 @@ watch(() => props.data,
 watch(() => props.layout,
   () => {
     innerLayout.value = cloneDeep(props.layout!)
-    schedule({ replot: false })
+    schedule({ replot: true })
   }
 )
 
