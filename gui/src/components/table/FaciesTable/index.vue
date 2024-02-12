21ecefd300e0946e6e4e0970f54e6208ac80bdde
Sindre Nistad <snis@equinor.com>
2024-02-12 11:45:20 +0100

    refactor: Separate component for the row; this makes it easier to make TypeScript happy

--- gui/src/components/table/FaciesTable/index.vue
+++ gui/src/components/table/FaciesTable/index.vue
@@ -1,7 +1,7 @@
 <template>
   <base-selection-table
-    v-model:current="currentId"
     v-model="selected"
+    :current="currentId"
     :items="(facies as GlobalFacies[])"
     :expanded="expanded"
     :headers="headers"
@@ -10,45 +10,13 @@
     :no-data-text="noDataText"
     :select-disabled="!canSelect"
     :select-error="selectFaciesError"
+    @update:current="value => currentId = value"
   >
-    <template #item="{ item: facies }: {item: GlobalFacies }">
-      <td class="dense">
-        <informational-icons :value="facies" :current="currentId" />
-      </td>
-      <td class="text-left">
-        <span v-if="isFaciesFromRms(facies)">
-          {{ facies.name }}
-        </span>
-        <editable-cell
-          v-else
-          :value="facies"
-          field="name"
-          @submit="name => facies.name = name.toString() || `F${facies.code}`"
-        />
-      </td>
-      <td v-if="!hideAlias" class="text-left">
-        <editable-cell
-          :value="facies"
-          field="alias"
-          @submit="(alias) => facies.alias = alias.toString()"
-        />
-      </td>
-      <td class="text-left">
-        <span v-if="isFaciesFromRms(facies)">
-          {{ facies.code }}
-        </span>
-        <editable-cell
-          v-else
-          :value="facies"
-          :restrictions="faciesCodeRestrictions(facies)"
-          field="code"
-          numeric
-          @submit="code => facies.code = typeof code === 'number' ? code : parseInt(code, 10)"
-        />
-      </td>
-      <td
-        :style="{ backgroundColor: facies.color, cursor: 'pointer' }"
-        @click.stop="() => changeColorSelection(facies)"
+    <template #item="{ item }">
+      <facies-row
+        :mode-value="item as GlobalFacies"
+        :expanded="expanded as GlobalFacies[]"
+        @expanded="items => expanded = items"
       />
     </template>
     <template #expanded-item="{ item, columns }">
@@ -66,10 +34,9 @@
 </template>
 
 <script setup lang="ts">
-import EditableCell from '@/components/table/EditableCell.vue'
 import BaseSelectionTable from '@/components/baseComponents/BaseSelectionTable.vue'
 import ColorPicker from '@/components/ColorPicker.vue'
-import InformationalIcons from '@/components/table/FaciesTable/InformationalIcons.vue'
+import FaciesRow from '@/components/table/FaciesTable/FaciesRow.vue'
 
 import type { Facies, GlobalFacies } from '@/utils/domain'
 
@@ -164,45 +131,8 @@ const selectFaciesError = computed(() => {
   return `A ${item} must be selected, before including a facies in the model`
 })
 
-function isFaciesFromRms(facies: GlobalFacies): boolean {
-  return faciesStore.isFromRMS(facies)
-}
-
-function faciesCodeRestrictions(
-  globalFacies: GlobalFacies,
-): ((code: string) => string)[] {
-  return [
-    (code: string): string => (!code ? 'A code cannot be empty' : ''),
-    (code): string => {
-      try {
-        Number.parseInt(code, 10)
-        return ''
-      } catch {
-        return 'Code must be an integer'
-      }
-    },
-    (code: string): string =>
-      Number.parseInt(code, 10) < 0 ? 'Code must be non-negative' : '',
-    (code: string): string =>
-      facies.value
-        .filter(({ id }) => globalFacies.id !== id)
-        .map(({ code }) => code.toString(10))
-        .includes(code)
-        ? 'Code is used by a different Facies'
-        : '',
-  ]
-}
-
 const availableColors = computed(
   () => colorStore.current?.colors ?? [],
 )
 
-function changeColorSelection(facies: GlobalFacies): void {
-  const previous = expanded.value.pop()
-  if (previous && previous.id === facies.id) {
-    expanded.value = []
-  } else {
-    expanded.value = [facies]
-  }
-}
 </script>
