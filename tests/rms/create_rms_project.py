6afb0c819abdcb7814586bbdb9dc78e7681b2623
Oddvar Lia (ST MSU GEO) <olia@equinor.com>
2025-03-06 12:28:22 +0100

    update

--- tests/rms/create_rms_project.py
+++ tests/rms/create_rms_project.py
@@ -2,10 +2,194 @@ import rmsapi
 import rmsapi.jobs
 import numpy as np
 import math
-
-# import xtgeo
+from typing import Any, Union
+import xtgeo
 import shutil
 from os.path import isdir
+from aps.utils.roxar.grid_model import create_zone_parameter
+from aps.utils.constants.simple import Debug
+
+ 
+def create_wells(
+        project: Any,
+        nwells: int,
+        well_name_prefix: str,
+        well_pos: list[tuple[float, float, float, float]],
+        nlogpoints: int,
+        facies_code_names: dict[int, str],
+        facies_intervals: list[list[tuple[float,int]]],
+        well_is_vertical: list[bool],
+        debug_level: Debug=Debug.OFF
+) -> list[str]:
+    
+    well_list = []
+    well_names = []
+    for well_indx in range(nwells):
+        # well head and name
+        well_name = f"{well_name_prefix}_{well_indx}"
+        well_names.append(well_name)
+        if debug_level > Debug.OFF:
+            print(f"Create well:  {well_name}")
+        well = project.wells.create(well_name)
+        well.rkb = 100
+        east = well_pos[well_indx][0]
+        north = well_pos[well_indx][1]
+        topdepth = well_pos[well_indx][2]
+        basedepth = well_pos[well_indx][3]
+        npoints = nlogpoints
+        loginc = (basedepth - topdepth) / npoints
+        well.well_head = (east, north)
+
+        if well_is_vertical[well_indx]:
+            # trajectory
+            trajectories = well.wellbore.trajectories
+            drilled_trajectory = trajectories.create("Drilled trajectory")
+            surveypoints = drilled_trajectory.survey_point_series
+            array_with_points = surveypoints.generate_survey_points(npoints)
+            array_with_points[:, 0] = np.arange(topdepth, basedepth, loginc)
+            array_with_points[:, 3].fill(east)
+            array_with_points[:, 4].fill(north)
+            array_with_points[:, 5] = np.arange(topdepth, basedepth, loginc)  # TVD
+            surveypoints.set_survey_points(array_with_points)
+
+            # log curves
+            log_run = drilled_trajectory.log_runs.create("Log run 1")
+            measured_depths = np.arange(
+                topdepth, basedepth, loginc
+            )  # MD points for log values
+            log_run.set_measured_depths(measured_depths)
+            nval = len(measured_depths)
+
+            facies_log_curve = log_run.log_curves.create_discrete("FACIES")
+            facies_values = np.zeros(nval, dtype=np.int32)
+
+            start_indx = 0
+            for interval in facies_intervals[well_indx]:
+                md_length, facies_code = interval
+                facies_values[start_indx:] = facies_code
+                indx_interval = int(md_length/loginc)
+                start_indx += indx_interval
+
+            facies_log_curve.set_values(facies_values)
+            code_names = facies_code_names
+            facies_log_curve.set_code_names(code_names)
+        else:
+            # trajectory
+            trajectories = well.wellbore.trajectories
+            drilled_trajectory = trajectories.create("Drilled trajectory")
+            surveypoints = drilled_trajectory.survey_point_series
+
+
+
+            points = [
+                # X            Y          TVD         MD         INCL         AZIMUTH
+                [1114.589,	-381.262,	2028.272],
+                [-1364.837,	413.633,	2105.073]
+            ]
+            npoints = len(points)
+            array_with_points = surveypoints.generate_survey_points(npoints)
+            # Calculate MD
+            x0, y0, z0 = points[0]
+            md_rel = 0.0
+            for indx in range(npoints):
+                pt = points[indx]
+                x = pt[0]
+                y = pt[1]
+                z = pt[2]
+                dx = x - x0
+                dy = y - y0
+                dz = z - z0
+                d = math.sqrt(dx*dx + dy*dy + dz*dz)
+                md_rel += d
+                array_with_points[indx, 0] = md_rel  # MD
+                array_with_points[indx, 3] = x  # X
+                array_with_points[indx, 4] = y  # Y
+                array_with_points[indx, 5] = z  # TVD
+                array_with_points[indx, 1] = 89.0  # INCL
+                array_with_points[indx, 2] = 300.0  # AXIMUTH
+                x0 = x
+                y0 = y
+                z0 = z
+                print(f"MD: {md_rel}")
+            surveypoints.set_survey_points(array_with_points)
+            md_max = array_with_points[npoints-1, 0]
+            md_min = array_with_points[0, 0]
+            md = np.arange(md_min,md_max, 25.0)
+            npoints_new = len(md)
+            new_array_with_points = np.zeros((npoints_new,6), dtype=np.float32)
+            for n in range(len(md)):
+                d = md[n]
+                new_array_with_points[n,:] = surveypoints.interpolate_survey_point(d)
+            surveypoints.set_survey_points(new_array_with_points)
+
+            # log curves
+            log_run = drilled_trajectory.log_runs.create("Log run 1")
+            measured_depths = np.arange(
+                md_min, md_max, loginc
+            )  # MD points for log values
+            log_run.set_measured_depths(measured_depths)
+            nval = len(measured_depths)
+
+            facies_log_curve = log_run.log_curves.create_discrete("FACIES")
+            facies_values = np.zeros(nval, dtype=np.int32)
+
+            start_indx = 0
+            for interval in facies_intervals[well_indx]:
+                md_length, facies_code = interval
+                facies_values[start_indx:] = facies_code
+                indx_interval = int(md_length/loginc)
+                start_indx += indx_interval
+
+            facies_log_curve.set_values(facies_values)
+            code_names = facies_code_names
+            facies_log_curve.set_code_names(code_names)
+
+    well_list.append(well)
+
+    return well_names
+
+
+def create_bw_job(
+    owner_strings: list[str],
+    job_type: str,
+    job_name: str,
+    well_names: list[str],
+    bw_set_name: str,
+    debug_level: Debug = Debug.OFF,
+):
+    bw_job = rmsapi.jobs.Job.create(owner=owner_strings, type=job_type, name=job_name)
+
+    params = {
+        "BlockedWellsName": bw_set_name,
+        "Discrete Blocked Log": [
+            {
+                "Name": "FACIES",
+                "CellLayerAveraging": False,
+            },
+        ],
+        "Wells": [["Wells", well_name] for well_name in well_names],
+    }
+    check, err_list, warn_list = bw_job.check(params)
+    if not check:
+        print("Error when creating blocked well job:")
+        for i in range(len(err_list)):
+            print(f"  {err_list[i]}")
+        print("Warnings when creating blocked well job:")
+        for i in range(len(warn_list)):
+            print(f"  {warn_list[i]}")
+
+    if debug_level > Debug.OFF:
+        print(
+            f"Create block well job:  {job_name} to make blocked well set: "
+            f"{bw_set_name}"
+        )
+        print(f"Use the wells: {well_names}")
+
+    bw_job.set_arguments(params)
+    bw_job.save()
+
+    return bw_job
+
 
 rms_project_name = 'aps_test.rmsxxx'
 
@@ -18,7 +202,6 @@ if isdir(rms_project_name):
 print('Create rms project')
 
 project = rmsapi.Project.create()
-# rox = xtgeo.RoxUtils(project)
 print('Create empty structural model object')
 struct_model_name = 'StructModel'
 model_box_center = (0.0, 0.0, -2045.0)  # Negative depth due to bug in rmsapi
@@ -31,8 +214,8 @@ z0 = rms_box[2]  # Have to switch sign due to a bug in rmsapi
 print(f'Origo:  ({x0}, {y0}, {z0})')
 model_box_origin = (x0, y0, z0)
 
-xinc = 50.0
-yinc = 50.0
+xinc = 10.0
+yinc = 10.0
 
 nx_surf = int(model_box_size[0] / xinc) + 1
 ny_surf = int(model_box_size[1] / yinc) + 1
@@ -260,8 +443,8 @@ params = {
     'BottomSurfaces': [[], [], [], []],
     'UseXInc': True,
     'UseYInc': True,
-    'XInc': 50.0,
-    'YInc': 50.0,
+    'XInc': xinc,
+    'YInc': yinc,
     'CellsPerZone': [-1, 15, 10, 25],
     'ClearExistingData': True,
     'ClipGrid': True,
@@ -293,27 +476,106 @@ grid_job.save()
 print(f'Run grid job: {job_name}')
 ok = grid_job.execute(0)
 
-# Zone parameter
-grid_model.properties.create("Zone",
-        property_type=rmsapi.GridPropertyType.discrete,
-        data_type=np.uint8)
-grid = grid_model.get_grid()
-dimensions = grid.simbox_indexer.dimensions
-nx, ny, nz = dimensions
-zonation = grid.get_zonation()
-
 # Define zone parameter values
-zone_values3d = np.zeros(dimensions,dtype=np.uint8)
-zone_number = 1
-code_names = {}
-for start_layer, zone_name in zonation.items():
-    zone_values3d[:,:,start_layer:] = zone_number
-    code_names[zone_number] = zone_name
-    zone_number += 1
-zone_values = zone_values3d.reshape(len(zone_values3d))
-grid_model.properties["Zone"].set_values(zone_values)
-zone_property = grid_model.properties["Zone"]
-zone_property.code_names = code_names
+zone_param = create_zone_parameter(
+    grid_model,
+    name="Zone")
+
+# Define some wells
+nwells = 4
+well_name_prefix = "Well"
+well_pos = [
+        (-635, 360, 1990, 2100),
+        (250, -730,1990, 2100),
+        (-500, -170, 1990, 2100),
+        (-618.455,     856.018,   1990,   2120.0),
+    ]
+well_is_vertical =[True, True, True, False]
+nlogpoints = 1000
+facies_code_names = {
+    1: "F1",
+    2: "F2",
+    5: "F5",
+    6: "F6",
+}
+facies_intervals = [
+        [
+        (5.0,  1),
+        (15.0, 2),
+        (15.0, 1),
+        (25.0, 6),
+        (5.0,  1),
+        (5.0,  5),
+        (15.0, 6),
+        (15.0, 2),
+        (25.0, 6),
+        (15.0, 2),
+        (5.0,  1),
+        ],
+        [
+        (7.0,  2),
+        (15.0, 1),
+        (10.0, 5),
+        (5.0,  6),
+        (10.0, 1),
+        (10.0,  2),
+        (10.0, 5),
+        (15.0, 2),
+        (15.0, 6),
+        (5.0, 1),
+        (5.0,  2),
+        ],
+        [
+        (15.0,  2),
+        (5.0, 1),
+        (15.0, 2),
+        (25.0, 5),
+        (5.0,  6),
+        (5.0,  5),
+        (15.0, 6),
+        (5.0,  2),
+        (15.0, 1),
+        (15.0, 2),
+        (5.0,  1),
+        ],
+        [
+        (50.0,  2),
+        (25.0, 1),
+        (135.0, 2),
+        (450.0, 5),
+        (550.0,  6),
+        (150.0,  5),
+        (350.0, 6),
+        (150.0,  2),
+        (250.0, 1),
+        (450.0, 2),
+        (250.0,  5),
+        (150.0,  2),
+        (350.0,  1),
+        (250.0,  5),
+        ],
+]
+well_list = create_wells(
+        project,
+        nwells,
+        well_name_prefix,
+        well_pos,
+        nlogpoints,
+        facies_code_names,
+        facies_intervals,
+        well_is_vertical,
+        debug_level=Debug.ON)
+
+owner_strings = ["Grid models", grid_model_name, "Grid"]
+bw_job =create_bw_job(
+        owner_strings,
+        "Block Wells",
+        "BW_job",
+        well_list,
+        "BW",
+        debug_level=Debug.ON)
+bw_job.execute()
+
 
 print('Save rms project and close')
 project.save_as(rms_project_name)
