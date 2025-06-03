ce72027b3f47610e7d4e06d5d5e541584f427909
Sindre Nistad <snis@equinor.com>
2025-06-03 16:03:05 +0200

    fix: Resolve unused imports
    Previously, importing `gaussianfft` before `roxar` / `rmsapi` would cause a segmentation fault but that has been resolved in `gaussianfft`

--- aps/algorithms/truncation_rules/__init__.py
+++ aps/algorithms/truncation_rules/__init__.py
@@ -1,3 +1,9 @@
 from aps.algorithms.truncation_rules.Trunc2D_Angle_xml import Trunc2D_Angle
 from aps.algorithms.truncation_rules.Trunc2D_Cubic_xml import Trunc2D_Cubic
 from aps.algorithms.truncation_rules.Trunc3D_bayfill_xml import Trunc3D_bayfill
+
+__all__ = [
+    'Trunc3D_bayfill',
+    'Trunc2D_Angle',
+    'Trunc2D_Cubic',
+]
